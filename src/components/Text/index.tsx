00ea0d5aea4b4cabab472aa8366017ef7c6fe220
Fernando Ferreira <fernandojbferreira@gmail.com>
2020-08-06 00:47:25 +0100

    fix: 🐛 fixed Text styles
    fixed error on Text color and variations styles

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,20 +1,22 @@
 import styled from 'styled-components';
-import { typography, TypographyProps } from 'styled-system';
+import { typography, TypographyProps, space, SpaceProps } from 'styled-system';
 
 import { ThemeProps } from '../../styles/theme';
 
-interface TextProps {
-  variant?: 'body' | 'small';
-  color?: 'primary' | 'secondary';
-}
+type TextProps = SpaceProps &
+  TypographyProps & {
+    variant?: 'body' | 'small';
+    color?: 'primary' | 'secondary';
+  };
 
-const Text = styled.span<TextProps & TypographyProps>`
-  ${typography}
-  color: ${({ variant, theme }: TextProps & ThemeProps) =>
-    variant === 'body' ? theme.colors.black : theme.colors.gray};
+const Text = styled.span<TextProps>`
+  color: ${({ color, theme }: TextProps & ThemeProps) =>
+    color === 'primary' ? theme.colors.black : theme.colors.gray};
   font-size: ${({ variant, theme }: TextProps & ThemeProps) =>
     variant === 'body' ? `${theme.fontSizes[2]}` : `${theme.fontSizes[1]}`};
   margin: 0;
+  ${space}
+  ${typography}
 `;
 
 Text.defaultProps = {
