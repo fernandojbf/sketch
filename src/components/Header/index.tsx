2630e68d1984e75ca336e458e1811f37c2c0dd0d
Fernando Ferreira <fernandojbferreira@gmail.com>
2020-08-06 00:49:15 +0100

    fix: 🐛 Added separator svg to Header
    Removed pseudo element background to use `separator.svg`

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,11 @@
 import { ReactNode, FunctionComponent } from 'react';
-import styled from 'styled-components';
 
 import Box from '../Box';
-
-import { ThemeProps } from '../../styles/theme';
 
 interface HeaderProps {
   content?: ReactNode;
   preContent?: ReactNode;
 }
-
-const PreContentWrapper = styled(Box)`
-  position: relative;
-  ::after {
-    content: '';
-    position: absolute;
-    top: 50%;
-    right: 0;
-    height: 50%;
-    width: 0;
-    border-radius: 50%;
-    transform: translateY(-50%);
-    box-shadow: ${({ theme }: ThemeProps) => theme.shadows.medium};
-  }
-`;
 
 const Header: FunctionComponent<HeaderProps> = ({ content, preContent }) => (
   <Box
@@ -33,9 +15,8 @@ const Header: FunctionComponent<HeaderProps> = ({ content, preContent }) => (
     py="0"
     position="relative"
   >
-    {preContent && (
-      <PreContentWrapper padding="3">{preContent}</PreContentWrapper>
-    )}
+    {preContent && <Box padding="3">{preContent}</Box>}
+    <Box as="img" height="40px" src={'/separator.svg'} />
     <Box px="4">{content}</Box>
   </Box>
 );
